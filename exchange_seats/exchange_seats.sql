9933e47f4b5845628dad0ccb2fd9241f075b8882
cgIIrw <chenguo_jay@163.com>
2018-07-18 20:19:47 +0800

    update exchange_seats

--- exchange_seats/exchange_seats.sql
+++ exchange_seats/exchange_seats.sql
@@ -1,15 +1,7 @@
-CREATE TABLE count01 (
-    counts1 int(11)
-);
-
-INSERT INTO count01
- ( counts1 )
-SELECT COUNT(*) FROM seat;
-
 SELECT
     CASE
-        WHEN id % 2 = 1 AND id != counts1  THEN id + 1
-        WHEN id % 2 = 0 AND id = counts1 THEN id
+        WHEN id % 2 = 1 AND id != (SELECT COUNT(*) FROM seat) THEN id + 1
+        WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM seat) THEN id
         ELSE id - 1
     END id, student
 FROM seat, count01
